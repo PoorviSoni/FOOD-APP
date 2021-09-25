7c2427e5bbaeeea843fccbb879209693236f44d8
poorvi <poorvisoni30@gmail.com>
2021-09-25 13:33:27 +0530

    authorization

--- src/AuthorizedTokens/AxiosRequestWithToken.js
+++ src/AuthorizedTokens/AxiosRequestWithToken.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
- function AxiosRequestWithToken() {
+function AxiosRequestWithToken() {
+  // get token
+  let token = localStorage.getItem("token");
+  let apiUrl = process.env.REACT_APP_TYPE
+    ? "https://myfoodlab.herokuapp.com/"
+    : "http://localhost:7000";
+  // add tokens to headers of req obj
+  let axiosReqWithToken = axios.create({
+    baseUrl: apiUrl,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-        // get token
-        let token=localStorage.getItem('token')
-        let apiUrl="http://localhost:7000"
-        // add tokents to headers of req obj
-        let axiosReqWithToken=axios.create({
-            baseUrl:apiUrl,
-            headers:{
-                Authorization: `Bearer ${token}`
-            }
-        })
-
-    return axiosReqWithToken;
+  return axiosReqWithToken;
 }
 
-export default AxiosRequestWithToken
+export default AxiosRequestWithToken;
